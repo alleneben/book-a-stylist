b9631c551aa659a817b0f0042d9852c83947c9df
Allen <alleneben@gmail.com>
2021-08-05 07:26:28 +0000

    :Import AppProvider to entire application

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,15 +3,18 @@ import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import { MenuProvider } from '../components/menu/menu_context';
+import { AppProvider } from '../api/appcontext';
 import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <MenuProvider>
-      <Component {...pageProps} />
-      </MenuProvider>
-    </ChakraProvider>
+    <AppProvider>
+      <ChakraProvider theme={theme}>
+        <MenuProvider>
+        <Component {...pageProps} />
+        </MenuProvider>
+      </ChakraProvider>
+    </AppProvider>
   )
 }
 
